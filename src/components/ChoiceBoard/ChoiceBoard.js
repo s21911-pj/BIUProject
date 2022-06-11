5226d08a35c6a084e77673e1b16d4416def69ad6
Piotr <s21911@pjwstk.edu.pl>
2022-06-11 23:27:33 +0200

    Removed the highlight on click for choice

--- src/components/ChoiceBoard/ChoiceBoard.js
+++ src/components/ChoiceBoard/ChoiceBoard.js
@@ -6,7 +6,7 @@ import { FaEraser } from "react-icons/fa";
 const CHOICES1 = [1, 2, 3, 4, 5];
 const CHOICES2 = [6, 7, 8, 9];
 
-const ChoiceBoard = ({ setClickValue,selected }) => {
+const ChoiceBoard = ({ setClickValue, selected }) => {
     return (
         <div className="ChoiceBoard">
             <div className="choice-container choices-one">
@@ -14,7 +14,7 @@ const ChoiceBoard = ({ setClickValue,selected }) => {
                     let selectedClass = choice === selected ? "selected" : "";
                     return (
                         <div
-                            className={`choice ${selectedClass}`}
+                            className={`choice ${selectedClass}noSelect`}
                             key={`key-1-${choice}`}
                             onClick={() => setClickValue(choice)}
                         >
@@ -28,7 +28,7 @@ const ChoiceBoard = ({ setClickValue,selected }) => {
                     let selectedClass = choice === selected ? "selected" : "";
                     return (
                         <div
-                            className={`choice ${selectedClass}`}
+                            className={`choice ${selectedClass}noSelect`}
                             key={`key-2-${choice}`}
                             onClick={() => setClickValue(choice)}
                         >
@@ -39,11 +39,13 @@ const ChoiceBoard = ({ setClickValue,selected }) => {
 
                 {/* Eraser class: value of zero */}
                 <div
-                    className={`choice ${selected === 0 ? "selected-eraser" : ""}`}
+                    className={`choice 
+                    ${selected === 0 ? "selected-eraser" : ""} 
+                    noSelect`}
                     key={`key-2-${0}`}
                     onClick={() => setClickValue(0)}
                 >
-                    <FaEraser/>
+                    <FaEraser />
                 </div>
             </div>
         </div>
--- src/components/InformationModal/InformationModal.js
+++ src/components/InformationModal/InformationModal.js
@@ -15,21 +15,17 @@ const InformationModal = ({ closeModal }) => {
                 </div>
                 <div className="modal-body">
                     <p>
-                        Welcome to the sudoku game, this game is developed as a Case Study
-                        of DM, it includes the sudoku generation and sudoku solver!
+                      Welcome
                     </p>
                     <div className="links">
-                        <a href="www.linkedin.com/in/suhaan-bhandary">LinkedIn</a>
-                        <a href="https://www.youtube.com/channel/UCHfmmdKuRDmZ5EUzGdqI7-Q">
-                            YouTube
-                        </a>
-                        <a href="https://github.com/Suhaan-Bhandary">GitHub</a>
+
+                        <a href="https://github.com/s21911-pj/BIUProject.git">GitHub</a>
                     </div>
                 </div>
                 <div className="modal-footer">
                     <Button
                         onClick={() =>
-                            window.open("https://github.com/Suhaan-Bhandary/Sudoku-Game")
+                            window.open("https://github.com/s21911-pj/BIUProject.git")
                         }
                         buttonStyle="btn--primary--solid"
                         text="GitHub"
